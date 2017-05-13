5cac8442a89c51b794d4ae5d18684b6dbe95e914
sirbimon <bimonaretga@outlook.com>
2017-05-13 07:08:19 -0400

    initial commit

---
+++ ShibaWeather/ShibaWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShibaWeather
+//
+//  Created by Bimonaretga on 5/13/17.
+//  Copyright © 2017 moeCodes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+
+}
+
